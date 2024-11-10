421a27b273ac9cf9338454c8858ae14e31923be4
kiadtiphong307 <kiaditphong.wm@gmail.com>
2024-11-10 21:05:20 +0700

    edit display v1.0.0

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div className="container mx-auto p-4 sm:p-6 md:p-8 lg:p-12 xl:p-16">
+        <div>
           {children}
         </div>
       </body>
